0172a9eb49c06c2e4e1ac171f5cc600af2011a2d
Llrac <112984468+Llrac@users.noreply.github.com>
2022-11-01 13:47:51 +0100

    More Player animations

--- 2 Close/Assets/Scripts/PlayerMove.cs
+++ 2 Close/Assets/Scripts/PlayerMove.cs
@@ -2,30 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 [RequireComponent(typeof(Rigidbody2D))]
 
 public class PlayerMove : MonoBehaviour
 {
+    Animator anim;
     Rigidbody2D rb;
 
-    float x;
-    float y;
+    bool isRunning = false;
+
+    float x, y;
     float diagonalSpeed = 0.7f;
 
     public float Speed = 10.0f;
 
     void Start()
     {
+        anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
-
+        
         rb.gravityScale = 0;
     }
 
     void Update()
     {
-        
-        x = Input.GetAxisRaw("Horizontal"); 
-        y = Input.GetAxisRaw("Vertical"); 
+        x = Input.GetAxisRaw("Horizontal");
+        y = Input.GetAxisRaw("Vertical");
+
+        if (rb.velocity.x < 0 && !isRunning)
+        {
+            isRunning = true;
+            anim.SetBool("isRunning", true);
+            anim.SetTrigger("goRun");
+        }
+        else if (rb.velocity.x > 0 && !isRunning)
+        {
+            isRunning = true;
+            anim.SetBool("isRunning", true);
+            anim.SetTrigger("goRun");
+        }
+        else
+        {
+            isRunning = false;
+            anim.SetBool("isRunning", false);
+            anim.SetTrigger("goIdle");
+        }
     }
 
     void FixedUpdate()
