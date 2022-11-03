df52f444b74b0f4182ba7f7a34d70d1277283de8
Llrac <112984468+Llrac@users.noreply.github.com>
2022-11-03 09:48:19 +0100

    GameManager is Singleton
    Player, Main Camera, Door & Key, Waypoints for enemies, and the Canvas are all children for GameManager. Removed prefabs from each child, because GameManager contains them all.

--- 2 Close/Assets/Scripts/GameManager.cs
+++ 2 Close/Assets/Scripts/GameManager.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
     public bool hasGoldenKey = false;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
