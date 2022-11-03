d6813328d63557a4c30643fc57bd26f34c41a5b0
JovinFast <112895166+JovinFast@users.noreply.github.com>
2022-11-03 10:09:56 +0100

    Show shadows when behind bookshelf

---
+++ 2 Close/Assets/Scripts/BehindBookShelfs.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehindBookShelfs : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("BehindBookshelf"))
+        {
+            spriteRenderer.enabled = true;
+        }
+        
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("BehindBookshelf"))
+        {
+            spriteRenderer.enabled = false;
+        }
+        
+    }
+}
